dac9564ff540f5d5ea078c233543558be62fd63f
rafael.delvalle <rafael.delvalle@spectec.net>
2022-11-25 16:33:29 -0300

    fix dahsboard and current

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ArticleDto } from '../components/models/article.model';
 import { HttpApiService } from '../services/http-api.service';
+import swal from'sweetalert2';
 
 @Component({
   selector: 'app-article',
@@ -30,5 +31,31 @@ export class ArticleComponent implements OnInit {
   delete(){
     console.log(this.article._id);
     this.httpApiService.deleteArticle(this.article._id)
+  }
+
+  deleteAlertConfirmation(){
+    swal.fire({
+      position: 'center',
+      title: 'Confirmar',
+      text: 'Desea eliminar este articulo? El cambio es irreversible.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'SI, eliminar.',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.delete();
+        swal.fire(
+          'Eliminado!',
+          'El artículo se eliminó.',
+          'success'
+        )
+      } else if (result.dismiss === swal.DismissReason.cancel) {
+        swal.fire(
+          'Cancelled',
+          'Item is safe.)',
+          'error'
+        )
+      }
+    })
   }
 }
